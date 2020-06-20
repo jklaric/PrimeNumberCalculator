522adf384e99bf02387bd9f1b475a7bb6be3f85c
jklaric <klaric9jakov@gmail.com>
2020-06-20 14:39:00 +0200

    inital setup

---
+++ PrimeNumberCalc/PrimeNumberCalc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumberCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            bool loopCtrl = true;
+
+            while (loopCtrl)
+            {
+                while (true)
+                {
+
+
+                    Console.WriteLine("Enter a number! If you would like to stop the process, just type finish.");
+                    int userInput = int.Parse(Console.ReadLine());
+
+                    if (userInput.GetType() == typeof(int))
+                    {
+                        numbers.Add(userInput);
+                    }
+                    else if (true)
+                    {
+                        loopCtrl = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please, enter a valid number or the finish command.");
+                    }
+
+                    break;
+
+                }
+                break;
+            }
+            Console.WriteLine(numbers[0]);
+
+                
+            }
+        }
+
+}
+
