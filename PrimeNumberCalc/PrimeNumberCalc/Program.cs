f5582e4df6a79f64879b8aa115d1d7e061619107
jklaric <klaric9jakov@gmail.com>
2020-06-30 13:26:51 +0200

    finished optimising and tweaking the code to prevent breaking

--- PrimeNumberCalc/PrimeNumberCalc/Program.cs
+++ PrimeNumberCalc/PrimeNumberCalc/Program.cs
@@ -12,8 +12,6 @@ namespace PrimeNumberCalc
             var isFinished = false;
             string userInput;
             int userInputAsInt;
-            int primeNumLowest;
-            int primeNumHighest;
             var primeNumbers = new List<int>();
             bool isPrime = true;
 
@@ -23,39 +21,54 @@ namespace PrimeNumberCalc
                 userInput = Console.ReadLine();
                 Console.WriteLine();
 
-                if (userInput.ToLower() == "finish")
+                if (userInput.ToLower() == "finish" && primeNumbers.Count > 2)
                     isFinished = true;
                 else if (Int32.TryParse(userInput, out userInputAsInt))
                 {
-                    if (TestForPrime(userInputAsInt, isPrime) == true)
+                    if (primeNumbers.Contains(userInputAsInt))
+                    {
+                        Console.WriteLine("Please enter a different number!");
+                    }
+
+                    else if (userInputAsInt > 1000)
+                    {
+                        Console.WriteLine("Please dont use numbers above 1000 to increase the calculator accuracy");
+                    }
+
+                    else if (IsPrimeNumber(userInputAsInt, isPrime))
                     {
                         primeNumbers.Add(userInputAsInt);
                     }
+
                 }
                 else
                     Console.WriteLine("Please enter a number or the word finish!");
             }
 
-            Console.WriteLine("If you would like to verify the result, the following is the result of " + primeNumbers.Min() + " raised to the power of " + primeNumbers.Max());
+
+            Console.WriteLine("If you would like to verify the result, the following is the result of " + primeNumbers.Min() + "  raised to the power of " + primeNumbers.Max());
 
             Console.WriteLine(Math.Pow(primeNumbers.Min(), primeNumbers.Max()));
 
         }
 
-        static bool TestForPrime(int userInputAsInt, bool isPrime)
+        static bool IsPrimeNumber(int userInputAsInt, bool isPrime)
         {
 
-
-            if (userInputAsInt % 2 == 0)
+            if (userInputAsInt == 1)
             {
                 isPrime = false;
             }
-            if (userInputAsInt % 3 == 0)
+            else if (userInputAsInt % 2 == 0 && userInputAsInt != 2)
+            {
+                isPrime = false;
+            }
+            else if (userInputAsInt % 3 == 0 && userInputAsInt != 3)
             {
                 isPrime = false;
             }
 
-            if (userInputAsInt % 5 == 0)
+            else if (userInputAsInt % 5 == 0 && userInputAsInt != 5)
             {
                 isPrime = false;
             }
